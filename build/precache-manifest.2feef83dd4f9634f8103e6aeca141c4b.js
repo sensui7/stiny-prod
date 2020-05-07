9d5b592fc6d2bd05bbac0501611a65465d2285d5
Steven Tran <transtev@pdx.edu>
2020-05-07 11:18:48 -0700

    fixing deployment

---
+++ build/precache-manifest.2feef83dd4f9634f8103e6aeca141c4b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238428dc0167ff977cdedb2d8b01c707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e1c62ba0afb47dccc6",
+    "url": "/static/css/2.0d592d09.chunk.css"
+  },
+  {
+    "revision": "6e7eb76830f0e3287e58",
+    "url": "/static/css/main.e64abdce.chunk.css"
+  },
+  {
+    "revision": "e4e1c62ba0afb47dccc6",
+    "url": "/static/js/2.568065d4.chunk.js"
+  },
+  {
+    "revision": "81c55e1bb8a6bbefaf9572bfc44297fe",
+    "url": "/static/js/2.568065d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e7eb76830f0e3287e58",
+    "url": "/static/js/main.05c1608d.chunk.js"
+  },
+  {
+    "revision": "0b9ebdd574abf601b7e0",
+    "url": "/static/js/runtime-main.9ae03ee9.js"
+  },
+  {
+    "revision": "8d68a31facd1b522b9dd7f359a244717",
+    "url": "/static/media/1.8d68a31f.jpg"
+  },
+  {
+    "revision": "6e2d959129ecbea17978f00695db9dba",
+    "url": "/static/media/2.6e2d9591.jpg"
+  },
+  {
+    "revision": "86e83c97af8d3340284b7a6c677f01a9",
+    "url": "/static/media/3.86e83c97.jpg"
+  },
+  {
+    "revision": "51c24f855420c2a4ea61e879045c3eb1",
+    "url": "/static/media/4.51c24f85.jpg"
+  },
+  {
+    "revision": "7baa8c77c3240a4560956a87df0e3144",
+    "url": "/static/media/5.7baa8c77.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "3ab90553a0281201367276235a178bb3",
+    "url": "/static/media/default.3ab90553.png"
+  },
+  {
+    "revision": "f642206b67707e549bd18159506c4ad5",
+    "url": "/static/media/office.f642206b.jpg"
+  }
+]);
