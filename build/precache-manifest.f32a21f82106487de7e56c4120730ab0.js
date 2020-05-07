848a0566d1899705496ee6ce806bf02bf249c39f
Steven Tran <transtev@pdx.edu>
2020-05-07 11:43:10 -0700

    adding missing build files

---
+++ build/precache-manifest.f32a21f82106487de7e56c4120730ab0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b77de928b94a6e52f72f36fdd18189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e774f68d0630f80a974",
+    "url": "/static/css/2.0d592d09.chunk.css"
+  },
+  {
+    "revision": "2cd269f539969d6ebeb0",
+    "url": "/static/css/main.582f88be.chunk.css"
+  },
+  {
+    "revision": "6e774f68d0630f80a974",
+    "url": "/static/js/2.a3e4a041.chunk.js"
+  },
+  {
+    "revision": "81c55e1bb8a6bbefaf9572bfc44297fe",
+    "url": "/static/js/2.a3e4a041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd269f539969d6ebeb0",
+    "url": "/static/js/main.5b8c4989.chunk.js"
+  },
+  {
+    "revision": "0b9ebdd574abf601b7e0",
+    "url": "/static/js/runtime-main.9ae03ee9.js"
+  },
+  {
+    "revision": "8d68a31facd1b522b9dd7f359a244717",
+    "url": "/static/media/1.8d68a31f.jpg"
+  },
+  {
+    "revision": "6e2d959129ecbea17978f00695db9dba",
+    "url": "/static/media/2.6e2d9591.jpg"
+  },
+  {
+    "revision": "86e83c97af8d3340284b7a6c677f01a9",
+    "url": "/static/media/3.86e83c97.jpg"
+  },
+  {
+    "revision": "51c24f855420c2a4ea61e879045c3eb1",
+    "url": "/static/media/4.51c24f85.jpg"
+  },
+  {
+    "revision": "7baa8c77c3240a4560956a87df0e3144",
+    "url": "/static/media/5.7baa8c77.jpg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "3ab90553a0281201367276235a178bb3",
+    "url": "/static/media/default.3ab90553.png"
+  },
+  {
+    "revision": "f642206b67707e549bd18159506c4ad5",
+    "url": "/static/media/office.f642206b.jpg"
+  }
+]);
